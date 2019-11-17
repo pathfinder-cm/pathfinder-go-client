0c38b2e2c69611d934ef64605e330e7b09030a83
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2019-11-18 05:35:24 +0700

    change bootstrap_attributes type as interface to handle uncertain type json

--- pfmodel/container.go
+++ pfmodel/container.go
@@ -1,30 +1,30 @@
 package pfmodel
 
 type Container struct {
-	Hostname     string  `json:"hostname"`
-	Ipaddress    string  `json:"ipaddress"`
-	NodeHostname string  `json:"node_hostname"`
-	Status       string  `json:"status"`
-	Source       Source  `json:"source"`
+	Hostname      string `json:"hostname"`
+	Ipaddress     string `json:"ipaddress"`
+	NodeHostname  string `json:"node_hostname"`
+	Status        string `json:"status"`
+	Source        Source `json:"source"`
 	Bootstrappers []Bootstrapper
 }
 
 type Source struct {
-	Type         string  `json:"source_type"`
-	Alias        string  `json:"alias"`
-	Mode         string  `json:"mode"`
-	Remote       Remote  `json:"remote"`
+	Type   string `json:"source_type"`
+	Alias  string `json:"alias"`
+	Mode   string `json:"mode"`
+	Remote Remote `json:"remote"`
 }
 
 type Remote struct {
-	Server       string `json:"server"`
-	Protocol     string `json:"protocol"`
-	AuthType     string `json:"auth_type"`
-	Certificate  string  `json:"certificate"`
+	Server      string `json:"server"`
+	Protocol    string `json:"protocol"`
+	AuthType    string `json:"auth_type"`
+	Certificate string `json:"certificate"`
 }
 
 type Bootstrapper struct {
-	Type string `json:"bootstrap_type"`
-	CookbooksUrl string `json:"bootstrap_cookbooks_url"`
-	Attributes string `json:"bootstrap_attributes"`
+	Type         string      `json:"bootstrap_type"`
+	CookbooksUrl string      `json:"bootstrap_cookbooks_url"`
+	Attributes   interface{} `json:"bootstrap_attributes"`
 }
